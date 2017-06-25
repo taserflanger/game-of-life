e5a75590e441a4f081c82e74426c930e750db348
taserflanger <pascal.nardi@gmail.com>
2017-06-25 13:28:11 +0200

    Commented case.js

--- case.js
+++ case.js
@@ -1,18 +1,19 @@
 function Case(x, y, state) {
+    // Der Constructor nimmt die Position, und ein status (0 tot, 1 lebend)
     this.x = x;
     this.y = y;
     this.state = state;
     this.next_state = state;
-    this.neighbours_count;
+    // next_state wird von der superClass (grid) berechnet, und wenn das grid
+    // sich aktualisiert, wird state die next_state
 }
 
 Case.prototype.render = function() {
-    push()
     var color = this.state == 1 ? 255 : 0;
-    stroke(100)
+    // Wenn die zelle am lebend ist wird die Color Weiss (255) sonst Schwartz (0)
+    stroke(100);
     fill(color);
     var size = 10;
     var offset = 1;
     rect(this.x*size, this.y*size, size-offset, size-offset);
-    pop()
 }
